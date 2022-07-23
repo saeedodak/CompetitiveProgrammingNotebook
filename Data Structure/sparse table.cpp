1ad9d7db2a678fd48b9433bfb31950e353113410
Saeed Odak <saeedodak@gmail.com>
2022-07-22 23:21:09 -0400

    Update sparse table.cpp

--- Data Structure/sparse table.cpp
+++ Data Structure/sparse table.cpp
@@ -1,17 +1,33 @@
-//precompute
-void sparse() {
-  //st[i][j] = min in range [i , i+2^j]
-  for(int j=0;j<MAXLOG;j++)
-    for(int i=0;i<n;i++) // 0-base
-      if(i+ (1<<j) -1<n) {
-        st[i][j]=(j ? min(st[i][j-1],st[i+(1<<j-1)][j-1]) : a[i]);
-	       cout<<i<<" "<<j<<" "<<st[i][j]<<endl;
+// usage:
+//   auto fun = [&](int i, int j) { return min(i, j); };
+//   SparseTable<int, decltype(fun)> st(a, fun);
+// or:
+//   SparseTable<int> st(a, [&](int i, int j) { return min(i, j); });
+template <typename T, class F = function<T(const T&, const T&)>>
+class SparseTable {
+ public:
+  int n;
+  vector<vector<T>> mat;
+  F func;
+
+  SparseTable(const vector<T>& a, const F& f) : func(f) {
+    n = static_cast<int>(a.size());
+    int max_log = 32 - __builtin_clz(n);
+    mat.resize(max_log);
+    mat[0] = a;
+    for (int j = 1; j < max_log; j++) {
+      mat[j].resize(n - (1 << j) + 1);
+      for (int i = 0; i <= n - (1 << j); i++) {
+        mat[j][i] = func(mat[j - 1][i], mat[j - 1][i + (1 << (j - 1))]);
       }
-}
-int query(int L,int R) {
-  int x=0,rem=R-L+1;
-  // x=log2(R-L+1);
-  while(rem>1)x++,rem>>=1;
-  cout<<x<<endl;
-  return min( st[L][x],st[R-(1<<x)][x]);
-}
+    }
+  }
+
+  T get(int from, int to) const {
+    assert(0 <= from && from <= to && to <= n - 1);
+    int lg = 32 - __builtin_clz(to - from + 1) - 1;
+    return func(mat[lg][from], mat[lg][to - (1 << lg) + 1]);
+  }
+};
+
+// tourist code!
